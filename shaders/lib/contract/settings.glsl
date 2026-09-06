b7e4d42e24de1a996032de5ec1eb67cadaaadc41
Huajijun <100221061+Huajijun01@users.noreply.github.com>
2026-09-06 14:33:54 +0800

    feat(exposure): expose target luminance

--- shaders/lib/contract/settings.glsl
+++ shaders/lib/contract/settings.glsl
@@ -28,6 +28,7 @@
 #define COLOR_DITHER_STRENGTH 1.0 // [0.0 0.25 0.5 0.75 1.0 1.25 1.5 2.0] Color dither strength: 0.5=conservative, 1.0=default, 2.0=aggressive
 
 //#define AE
+#define AE_TARGET_LUMINANCE 0.18 // [0.08 0.10 0.12 0.15 0.18 0.21 0.24 0.28 0.32 0.40] Auto-exposure target scene-linear luminance
 
 #define CAS
 #define CAS_SHARPNESS 0.75 // [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
